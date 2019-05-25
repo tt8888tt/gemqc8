5c4b6112b37f8c0e911a2729e96024aadb38f25f
Giovanni Mocellin <gio.moc@gmail.com>
2019-05-25 10:44:26 +0200

    Excel <-> csv files conversion updated/created

--- Analysis/GEMQC8/python/csv_to_excel.py
+++ Analysis/GEMQC8/python/csv_to_excel.py
@@ -4,39 +4,26 @@ import os, sys, io
 
 def conversion(in_name):
 
-	if in_name.endswith('.xls'):
-		with xlrd.open_workbook(in_name) as infile:
-			sh = infile.sheet_by_index(0)  # or wb.sheet_by_name('name_of_the_sheet_here')
-			out_name = in_name[:-4] + '.csv'
-			if sys.version_info[0] == 2: # python2
-				with open(out_name, 'wb') as outfile:
-					c = csv.writer(outfile)
-					for r in range(sh.nrows):
-						c.writerow(sh.row_values(r))
-			if sys.version_info[0] == 3: # python3
-				with open(out_name, 'w', newline="") as outfile:
-					c = csv.writer(outfile)
-					for r in range(sh.nrows):
-						c.writerow(sh.row_values(r))
+	if in_name.endswith('.csv'):
+		wb = openpyxl.Workbook()
+		ws = wb.active
 
-	if in_name.endswith('.xlsx'):
-		infile = openpyxl.load_workbook(in_name)
-		sh = infile.active
-		out_name = in_name[:-5] + '.csv'
-		if sys.version_info[0] == 2: # python2
-			with open(out_name, 'wb') as outfile:
-				c = csv.writer(outfile)
-				for r in sh.rows:
-					c.writerow([cell.value for cell in r])
-		if sys.version_info[0] == 3: # python3
-			with open(out_name, 'w', newline="") as outfile:
-				c = csv.writer(outfile)
-				for r in sh.rows:
-					c.writerow([cell.value for cell in r])
+		with open(in_name) as infile:
+		    reader = csv.reader(infile, delimiter=',')
+		    for row in reader:
+		        ws.append(row)
 
-	print("\n")
-	print("Success: " + in_name + " converted to " + outname)
-	print("\n")
+		out_name = in_name[:-4] + '.xslx'
+		wb.save(out_name)
+
+		print("\n")
+		print("Success: " + in_name + " converted to " + outname)
+		print("\n")
+
+	else:
+		print("\n")
+		print("The input file does not have the correct extension!")
+		print("\n")
 
 if __name__ == '__main__':
     input_file = sys.argv[1]
