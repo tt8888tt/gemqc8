be18a9fc6f136d26c917753584aad33a569f1d69
Giovanni Mocellin <gio.moc@gmail.com>
2019-06-13 17:02:48 +0200

    Modified code to dump DB tables After we get the table modified by the DB guys, to get the stand configuration table we need to search for the one that is the closest in time. This has been implemented, but needs to be tested when a table like that can be created in the DB

--- Analysis/GEMQC8/python/dumpDBtables.py
+++ Analysis/GEMQC8/python/dumpDBtables.py
@@ -1,13 +1,34 @@
 import cx_Oracle
 import os, sys, io
+from date_time_runInfoDB import startDateTime
 
-def getConfigurationTable(run_num):
+def getConfigurationTable(run_num,userRunInfoDB_add):
     print "\nDownloading ConfigurationTable for run {0}".format(run_num)
+
+    runDateTime = date_time_runInfoDB.startDateTime(run_num,userRunInfoDB_add)
 
     db = cx_Oracle.connect('GEM_904_COND/904CondDB@INT2R')
     cur = db.cursor()
 
-    query = "select * from CMS_GEM_MUON_VIEW.QC8_GEM_STAND_GEOMETRY_VIEW_RH where RUN_NUMBER="+str(run_num)
+    delta_min = 94608000 # minimum value of delta (this value is the # of seconds in 3 years)
+    goodDateTimeForQuery = ""
+
+    query = "select TIME from CMS_GEM_MUON_VIEW.QC8_GEM_STAND_GEOMETRY_VIEW_RH"
+    cur.execute(query)
+    for result in cur:
+        standConfigurationDateTime = result[0]
+        year = standConfigurationDateTime.split('-')[0]
+        month = standConfigurationDateTime[5:].split('-')[0]
+        day = standConfigurationDateTime[8:].split(' ')[0]
+        hour = standConfigurationDateTime[11:].split(':')[0]
+        minutes = standConfigurationDateTime[14:].split(':')[0]
+        seconds = standConfigurationDateTime[17:]
+        deltaT = (runDateTime - datetime.datetime(int(year),int(month),int(day),int(hour),int(minutes),int(seconds))).total_seconds()
+        if (deltaT >= 0 and deltaT < delta_min):
+            delta_min = deltaT
+            goodDateTimeForQuery = standConfigurationDateTime
+
+    query = "select * from CMS_GEM_MUON_VIEW.QC8_GEM_STAND_GEOMETRY_VIEW_RH where TIME="+str(goodDateTimeForQuery)
     cur.execute(query)
 
     configTablesPath = os.path.abspath("dumpDBtables.py").split('QC8Test')[0] + 'QC8Test/src/Analysis/GEMQC8/data/StandConfigurationTables/'
@@ -25,7 +46,8 @@ def getConfigurationTable(run_num):
             amc          = result[5]
             oh           = result[6]
             flow_meter   = result[7]
-            run_number   = result[8]
+            time         = result[8]
+            run_number   = run_num
             line = str(chamber_name) + "," + str(gem_num) + "," + str(position) + "," + str(ch_type) + "," + str(flip) + "," + str(amc) + "," + str(oh) + "," + str(flow_meter) + "," + str(run_number) + "\n"
             outfile.write(line)
 
@@ -119,8 +141,9 @@ def getDeadStripsTable(run_num):
 if __name__ == '__main__':
     runNumber = sys.argv[1]
     tableType = sys.argv[2]
+    userDB_add = sys.argv[3]
     if tableType == "ConfigurationTable":
-        getConfigurationTable(runNumber)
+        getConfigurationTable(runNumber,userDB_add)
     elif tableType == "AlignmentTable":
         getAlignmentTable(runNumber)
     elif tableType == "HotStripsTable":
